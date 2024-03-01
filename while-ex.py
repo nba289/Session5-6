cfcbda8250ae26521f640bc12a8b7047e26ad4c5
nbonn <nbonnalgueal@gmail.com>
2024-03-01 13:01:21 +0100

    First Commit

---
+++ while-ex.py
@@ -0,0 +1,16 @@
+import random
+lives = 3
+while lives:
+    print("You have", lives, "lives left.")
+    #roll the dice
+    dice = random.randint(1, 6)
+    print("You rolled a", dice)
+    #check if you win
+    if dice == 6:
+        print("You win!")
+        break
+    lives -= 1
+else:
+    print("\n\nYOU LOSE!\n\n")
+
+print("Thank you for playing. Goodbye.")
